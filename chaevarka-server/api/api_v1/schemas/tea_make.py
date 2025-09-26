a2ed1ee5201d72fb61e3ccc701a89d184bd6a1bc
Olga Avduhina <olgaavduhina06@gmail.com>
2025-09-26 15:04:25 +0300

    add schemas for tea_make

---
+++ chaevarka-server/api/api_v1/schemas/tea_make.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from typing import Annotated
+from pydantic import BaseModel, Field
+
+class TeaMakeCreate(BaseModel):
+   device_id: str
+   water: Annotated[int, Field(ge=50, le=300)]
+   temperature: Annotated[int, Field(ge=70, le=100)]
+   sugar: Annotated[int, Field(ge=0, le=5)]
+   type: Annotated[int, Field(ge=0, le=6)]
+   time: Annotated[int, Field(ge=0, le=5)]
+   tea_cnt: Annotated[int, Field(ge=0, le=10)]
+
+class TeaMakeResponse(TeaMakeCreate):
+   id: int
+   status: str
+   created_at: datetime
+
+
+   class Config:
+       from_attributes = True
