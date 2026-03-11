6cf5207913b3a156a9aec3a6830acbeb7b42a1bf
Olga Avduhina <olgaavduhina06@gmail.com>
2026-03-11 16:44:21 +0300

    add online_at in device models

--- chaevarka-server/core/models/device.py
+++ chaevarka-server/core/models/device.py
@@ -1,7 +1,9 @@
 from sqlalchemy.orm import Mapped, mapped_column
+from datetime import datetime
 
 from .base import Base
 class Device(Base):
     __tablename__ = "devices"
     device_id: Mapped[str] = mapped_column(unique=True)
     ip_address: Mapped[str] = mapped_column()
+    online_at: Mapped[datetime] = mapped_column(nullable=False)
