4a4422948d33d5cbd3fe32207ab469fee5e42380
Olga Avduhina <olgaavduhina06@gmail.com>
2026-03-15 10:43:08 +0300

    create user-manager

--- chaevarka-server/api/dependencies/user_manager.py
+++ chaevarka-server/api/dependencies/user_manager.py
@@ -1,3 +1,7 @@
+from fastapi import Depends
+from api.dependencies.users import get_user_db
+from core.authentication.user_manager import UserManager
+
 
 async def get_user_manager(user_db=Depends(get_user_db)):
     yield UserManager(user_db)
