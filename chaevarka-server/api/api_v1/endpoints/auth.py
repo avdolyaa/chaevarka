7bfa0e3c45c91d67aa03b0ee87f1da3ea0b68e70
Olga Avduhina <olgaavduhina06@gmail.com>
2026-03-15 11:18:51 +0300

    include auth router from fastapi users

--- chaevarka-server/api/api_v1/endpoints/auth.py
+++ chaevarka-server/api/api_v1/endpoints/auth.py
@@ -1,10 +1,15 @@
 from fastapi import APIRouter
+
+from api.dependencies.authentication.backend import authentication_backend
+from api.dependencies.authentication.fastapi_users import fastapi_users
 from core.config import settings
-
-
 
 router = APIRouter(
     prefix=settings.api.prefix,
     tags=["Auth"]
 )
 
+router.include_router(
+    router=fastapi_users.get_auth_router(authentication_backend),
+    prefix="/auth/jwt",
+)
