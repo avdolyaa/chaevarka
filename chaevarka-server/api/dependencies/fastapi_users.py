45f8391124ffe796aa021c464d8b670fa85cb178
Olga Avduhina <olgaavduhina06@gmail.com>
2026-03-15 10:53:51 +0300

    create fastapi_users router

---
+++ chaevarka-server/api/dependencies/fastapi_users.py
@@ -0,0 +1,12 @@
+import uuid
+
+from fastapi_users import FastAPIUsers
+
+from api.dependencies.backend import authentication_backend
+from api.dependencies.user_manager import get_user_manager
+from core.models import User
+
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [authentication_backend],
+)
