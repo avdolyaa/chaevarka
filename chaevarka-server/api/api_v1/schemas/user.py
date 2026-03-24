8ce23fdf752e5fb0112cc5d0a2ba76a028491ac8
Olga Avduhina <olgaavduhina06@gmail.com>
2026-03-24 22:52:17 +0300

    change users model

--- chaevarka-server/api/api_v1/schemas/user.py
+++ chaevarka-server/api/api_v1/schemas/user.py
@@ -1,15 +1,30 @@
-import uuid
+from typing import Optional
 
 from fastapi_users import schemas
+from pydantic import ConfigDict, Field
 
 
 class UserRead(schemas.BaseUser[int]):
-    pass
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
+    phone: Optional[str] = None
+    profile_picture: Optional[str] = None
+    device_id: Optional[str] = None
+
+    model_config = ConfigDict(
+        from_attributes=True,
+        populate_by_name=True
+    )
 
 
 class UserCreate(schemas.BaseUserCreate):
-    pass
+    first_name: str = Field(..., min_length=2)
+    last_name: str = Field(..., min_length=2)
 
 
 class UserUpdate(schemas.BaseUserUpdate):
-    pass
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
+    phone: Optional[str] = None
+    profile_picture: Optional[str] = None
+    device_id: Optional[str] = None
