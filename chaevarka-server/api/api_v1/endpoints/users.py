2f820665d81722ca86561f58543e7f33a8b26256
Olga Avduhina <olgaavduhina06@gmail.com>
2026-06-25 17:39:43 +0300

    add delete/me

--- chaevarka-server/api/api_v1/endpoints/users.py
+++ chaevarka-server/api/api_v1/endpoints/users.py
@@ -1,7 +1,10 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from starlette import status
 
 from api.api_v1.schemas.user import UserRead, UserUpdate
 from api.dependencies.authentication.fastapi_users import fastapi_users
+from api.dependencies.authentication.auth import current_active_user
+from api.dependencies.authentication.user_manager import get_user_manager
 from core.config import settings
 
 router = APIRouter(
@@ -13,4 +16,11 @@ router.include_router(
     fastapi_users.get_users_router(UserRead, UserUpdate),
     prefix="/users",
 )
+
+@router.delete("/users/me", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_me(
+    current_user=Depends(current_active_user),
+    user_manager=Depends(get_user_manager),
+):
+    await user_manager.delete(current_user)
 
